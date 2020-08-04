5866e97c5be4a9230b544949227bb6468674f0c5
Thomas Antonini <antonini.thomas@gmail.com>
2020-08-04 13:27:15 +0200

    system-setup: Add main menu
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- system-setup.sh
+++ system-setup.sh
@@ -3,3 +3,20 @@
 REALPATH="$(readlink -e "$0")"
 BASEDIR="$(dirname "${REALPATH}")"
 
+WT_HEIGHT=40
+WT_WIDTH=120
+WT_MENU_HEIGHT=20
+
+while true; do
+    FUN=$(whiptail --title "System Configuration" --menu "Setup Options" \
+          "$WT_HEIGHT" "$WT_WIDTH" "$WT_MENU_HEIGHT" \
+          --cancel-button Finish --ok-button Select -- \
+          "1 " "Dummy" \
+          3>&1 1>&2 2>&3)
+
+    RET=$?
+    if [ $RET -eq 1 ]; then
+        exit 0
+    fi
+done
+
